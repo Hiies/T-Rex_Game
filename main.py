a70068d9d58eae2aca96c3c4c5bb8a35e0dd36f1
Hiran Whites <hiran.whites@hotmail.com>
2021-12-07 22:04:15 -0300

    Adicionando obstáculos e colisão

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import os
 import random
 
+from pygame.constants import KEYDOWN
+
 pygame.init()
 
 tela_largura = 1100
@@ -67,6 +69,7 @@ class Dino:
             self.dino_abaixado = False
             self.dino_correndo = False
             self.dino_pulando = True
+
         elif userInput[pygame.K_DOWN] and not self.dino_pulando:
             self.dino_abaixado = True
             self.dino_correndo = False
@@ -121,8 +124,52 @@ class Nuvem:
         tela.blit(self.image, (self.x, self.y))
 
 
+class Obstaculo:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = tela_largura
+
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < - self.rect.width:
+            obstaculos.pop()
+
+    def draw(self, tela):
+        tela.blit(self.image[self.type], self.rect)
+
+
+class SmallCactus(Obstaculo):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+
+
+class LargeCactus(Obstaculo):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 300
+
+
+class Passaro(Obstaculo):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 250
+        self.index = 0
+
+    def draw(self, tela):
+        if self.index >= 9:
+            self.index = 0
+        tela.blit(self.image[self.index//5], self.rect)
+        self.index += 1
+
+
 def main():
-    global game_speed, x_pos_bg, y_pos_bg, pontos
+    global game_speed, x_pos_bg, y_pos_bg, pontos, obstaculos
     run = True
     relogio = pygame.time.Clock()
     player = Dino()
@@ -132,6 +179,8 @@ def main():
     font = pygame.font.Font('freesansbold.ttf', 20)
     x_pos_bg = 0
     y_pos_bg = 380
+    obstaculos = []
+    death_count = 0
 
     def pontuacao():
         global pontos, game_speed
@@ -158,11 +207,29 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                quit()
         userInput = pygame.key.get_pressed()
         tela.fill((255, 255, 255))
 
         player.draw(tela)
         player.update(userInput)
+
+        if len(obstaculos) == 0:
+            if random.randint(0, 2) == 0:
+                obstaculos.append(SmallCactus(cacto_pequeno))
+            elif random.randint(0, 2) == 1:
+                obstaculos.append(LargeCactus(cacto_grande))
+            elif random.randint(0, 2) == 2:
+                obstaculos.append(Passaro(passaro))
+
+        for obstaculo in obstaculos:
+            obstaculo.draw(tela)
+            obstaculo.update()
+            if player.dino_rect.colliderect(obstaculo.rect):
+                pygame.time.delay(500)
+                death_count += 1
+                menu(death_count)
 
         nuvem.draw(tela)
         nuvem.update()
@@ -175,4 +242,37 @@ def main():
         pygame.display.update()
 
 
-main()
+def menu(death_count):
+    global pontos
+    run = True
+    while run:
+        tela.fill((255, 255, 255))
+        font = pygame.font.Font('freesansbold.ttf', 30)
+
+        if death_count == 0:
+            text = font.render(
+                "Pressione qualquer tecla para começar", True, (0, 0, 0))
+        elif death_count > 0:
+            text = font.render(
+                "Pressione qualquer tecla para recomeçar", True, (0, 0, 0))
+            score = font.render("Sua pontuação: " +
+                                str(pontos), True, (0, 0, 0))
+            scoreRect = score.get_rect()
+            scoreRect.center = (tela_largura // 2, tela_altura // 2 + 50)
+            tela.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (tela_largura // 2, tela_altura // 2)
+        tela.blit(text, textRect)
+        tela.blit(correndo[0], (tela_largura // 2 -
+                                20, tela_altura // 2 - 140))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                main()
+
+
+menu(death_count=0)
